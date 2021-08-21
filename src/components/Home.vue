98a5d078e84f947dfc175243a5cf7ee88e7f7d77
sseunie <stefy.ms97@gmail.com>
2021-08-21 19:56:15 +0100

    json-server and axios installed

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  {{ notifications }}
+</template>
+
+<script>
+import globalAxios from 'axios';
+
+export default {
+  name: 'Home',
+  data: () => ({
+    notifications: [],
+  }),
+  created() {
+    globalAxios.get(`http://localhost:3000/avisos`)
+        .then(r => {
+          this.notifications = r.data
+        })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
