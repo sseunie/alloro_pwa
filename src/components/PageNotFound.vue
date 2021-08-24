405bbeeec147f04ca1e74152621cd185b1961f10
sseunie <stefy.ms97@gmail.com>
2021-08-24 15:51:29 +0100

    PageNotFound component added for HTTP 404

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page-content">
+        <div class="page-title page-title-small">
+            <h2><a href="#" data-back-button><i class="fa fa-arrow-left"></i></a>Error 404</h2>
+        </div>
+        <div class="card header-card shape-rounded" style="height: 150px;">
+            <div class="card-overlay bg-highlight opacity-95"></div>
+            <div class="card-overlay dark-mode-tint"></div>
+        </div>
+
+        <div class="card card-style text-center">
+            <div class="content pt-4 pb-4">
+                <h1><i class="fa fa-exclamation-triangle color-red-dark fa-5x"></i></h1>
+                <h1 class="fa-6x pt-5 pb-2">404</h1>
+                <h3 class="text-uppercase pb-3">Page not Found</h3>
+                <p class="boxed-text-l">
+                    La página que buscas no existe.
+                </p>
+                <div class="row">
+                    <router-link to="/homepage" class="back-button btn btn-m btn-center-m bg-highlight font-800 text-uppercase">Inicio</router-link>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PageNotFound"
+}
+</script>
+
+<style scoped>
+</style>
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import LandingPage from "@/components/LandingPage";
 import HomePage from "@/components/HomePage";
+import PageNotFound from "@/components/PageNotFound";
 
 const routes = [
     { path: '/', component: LandingPage },
-    { path: '/homepage', component: HomePage }
+    { path: '/homepage', component: HomePage },
+    { path: '/:pathMatch(.*)*', component: PageNotFound }
 ];
 
 const router = createRouter({
